218116c359e86608c250fc02ad061159753eb719
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 04:34:24 +0300

    Зроблено клас Character.cs для завдання 5

---
+++ lab-2/Lab2/Builder/Character.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public class Character
+    {
+        public double Height { get; set; }
+        public string BodyType { get; set; }
+        public string HairColor { get; set; }
+        public string EyeColor { get; set; }
+        public string Clothing { get; set; }
+        public List<string> Inventory { get; set; }
+
+        public Character()
+        {
+            Inventory = new List<string>();
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Зріст: {Height}");
+            Console.WriteLine($"Статура: {BodyType}");
+            Console.WriteLine($"Колір волосся: {HairColor}");
+            Console.WriteLine($"Колір очей: {EyeColor}");
+            Console.WriteLine($"Одяг: {Clothing}");
+            Console.WriteLine("Інвентар: " + string.Join(", ", Inventory));
+        }
+    }
+}
+
