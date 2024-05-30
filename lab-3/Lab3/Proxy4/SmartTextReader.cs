289ecaa870fd8265028b52e48a49ca48d03ecbaa
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 16:49:12 +0300

    Створено  клас SmartTextReader.cs для завдання 4

---
+++ lab-3/Lab3/Proxy4/SmartTextReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Proxy4
+{
+    public class SmartTextReader : ITextReader
+    {
+        public string FilePath { get; }
+
+        public SmartTextReader(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        public char[][] ReadFile()
+        {
+            string[] lines = File.ReadAllLines(FilePath);
+            char[][] result = new char[lines.Length][];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                result[i] = lines[i].ToCharArray();
+            }
+
+            return result;
+        }
+    }
+}
