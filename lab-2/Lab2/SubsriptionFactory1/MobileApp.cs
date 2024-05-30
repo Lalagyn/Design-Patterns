a51474c68adbe6f4352a68bc490e9a9c340c8bad
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 04:24:37 +0300

    Зроблено клас MobileApp.cs для завдання 1

---
+++ lab-2/Lab2/SubsriptionFactory1/MobileApp.cs
@@ -0,0 +1,20 @@
+﻿namespace SubscriptionFactory
+{
+    public class MobileApp
+    {
+        public Subscription CreateSubscription(string type)
+        {
+            switch (type)
+            {
+                case "Domestic":
+                    return new DomesticSubscription("MobileApp");
+                case "Educational":
+                    return new EducationalSubscription("MobileApp");
+                case "Premium":
+                    return new PremiumSubscription("MobileApp");
+                default:
+                    throw new ArgumentException("Невідомий тип підписки");
+            }
+        }
+    }
+}
