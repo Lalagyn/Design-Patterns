41489646763d7607b82da7e9080f09a8ab201f83
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 04:34:39 +0300

    Зроблено клас CharacteDirectorr.cs для завдання 5

---
+++ lab-2/Lab2/Builder/CharacterDirector.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public class CharacterDirector
+    {
+        private ICharacterBuilder _builder;
+
+        public CharacterDirector(ICharacterBuilder builder)
+        {
+            _builder = builder;
+        }
+
+        public Character ConstructHero()
+        {
+            return _builder
+                .SetHeight(150)
+                .SetBodyType("Варвар")
+                .SetHairColor("Блондин")
+                .SetEyeColor("Жовтий")
+                .SetClothing("Лицарські обладунки")
+                .AddInventoryItem("Меч")
+                .AddInventoryItem("Щит")
+                .Build();
+        }
+
+        public Character ConstructEnemy()
+        {
+            return _builder
+                .SetHeight(140)
+                .SetBodyType("Лучница")
+                .SetHairColor("Розовий")
+                .SetEyeColor("Червоні")
+                .SetClothing("Лучниця мантія")
+                .AddInventoryItem("Лук")
+                .AddInventoryItem("Вогняні стріли")
+                .Build();
+        }
+    }
+}
