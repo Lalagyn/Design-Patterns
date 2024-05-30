b477c682f1cf56dc3e307cb79e5c450d77135c2e
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 04:29:31 +0300

    Зроблено клас Authenticator.cs для завдання 3

---
+++ lab-2/Lab2/Singleton3/Authenticator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    public sealed class Authenticator
+    {
+        private static Authenticator instance = null;
+        private static readonly object padlock = new object();
+
+        private Authenticator()
+        {
+            // Приватний конструктор для запобігання створенню екземплярів ззовні
+        }
+
+        public static Authenticator Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Authenticator();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void Authenticate(string username, string password)
+        {
+            // Логіка аутентифікації
+            Console.WriteLine($"Authenticating user {username}");
+        }
+    }
+}
