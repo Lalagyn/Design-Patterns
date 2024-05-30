42c1e02cbc997e387acc4000860604f4bf8038f0
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 04:25:00 +0300

    Зроблено клас ManagerCall.cs для завдання 1

---
+++ lab-2/Lab2/SubsriptionFactory1/ManagerCall.cs
@@ -0,0 +1,20 @@
+﻿namespace SubscriptionFactory
+{
+    public class ManagerCall
+    {
+        public Subscription CreateSubscription(string type)
+        {
+            switch (type)
+            {
+                case "Domestic":
+                    return new DomesticSubscription("ManagerCall");
+                case "Educational":
+                    return new EducationalSubscription("ManagerCall");
+                case "Premium":
+                    return new PremiumSubscription("ManagerCall");
+                default:
+                    throw new ArgumentException("Невідомий тип підписки");
+            }
+        }
+    }
+}
