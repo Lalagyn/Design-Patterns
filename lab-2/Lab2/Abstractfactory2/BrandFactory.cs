96f5b066c15c1ca30a6e93dff94d9891c984972c
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 04:27:37 +0300

    Зроблено клас BrandFactory.cs для завдання 2

---
+++ lab-2/Lab2/Abstractfactory2/BrandFactory.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Abstractfactory
+{
+    public interface IBrandFactory
+    {
+        IDevice CreateLaptop();
+        IDevice CreateNetbook();
+        IDevice CreateEBook();
+        IDevice CreateSmartphone();
+    }
+
+    public class IProneFactory : IBrandFactory
+    {
+        public IDevice CreateLaptop()
+        {
+            return new Laptop("IProne");
+        }
+
+        public IDevice CreateNetbook()
+        {
+            return new Netbook("IProne");
+        }
+
+        public IDevice CreateEBook()
+        {
+            return new EBook("IProne");
+        }
+
+        public IDevice CreateSmartphone()
+        {
+            return new Smartphone("IProne");
+        }
+    }
+
+    public class KiaomiFactory : IBrandFactory
+    {
+        public IDevice CreateLaptop()
+        {
+            return new Laptop("Kiaomi");
+        }
+
+        public IDevice CreateNetbook()
+        {
+            return new Netbook("Kiaomi");
+        }
+
+        public IDevice CreateEBook()
+        {
+            return new EBook("Kiaomi");
+        }
+
+        public IDevice CreateSmartphone()
+        {
+            return new Smartphone("Kiaomi");
+        }
+    }
+
+    public class BalaxyFactory : IBrandFactory
+    {
+        public IDevice CreateLaptop()
+        {
+            return new Laptop("Balaxy");
+        }
+
+        public IDevice CreateNetbook()
+        {
+            return new Netbook("Balaxy");
+        }
+
+        public IDevice CreateEBook()
+        {
+            return new EBook("Balaxy");
+        }
+
+        public IDevice CreateSmartphone()
+        {
+            return new Smartphone("Balaxy");
+        }
+    }
+}
