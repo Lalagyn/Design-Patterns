38e3c2a731567b4e941023f8ed8c3f3f0129bdd7
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 04:23:55 +0300

    Зроблено клас Website.cs для завдання 1

---
+++ lab-2/Lab2/SubsriptionFactory1/Website.cs
@@ -0,0 +1,20 @@
+﻿namespace SubscriptionFactory
+{
+    public class WebSite
+    {
+        public Subscription CreateSubscription(string type)
+        {
+            switch (type)
+            {
+                case "Domestic":
+                    return new DomesticSubscription("WebSite");
+                case "Educational":
+                    return new EducationalSubscription("WebSite");
+                case "Premium":
+                    return new PremiumSubscription("WebSite");
+                default:
+                    throw new ArgumentException("Невідомий тип підписки");
+            }
+        }
+    }
+}
