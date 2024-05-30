324f5920c9d5722c4a716fb1ae8b2ee3fc2b56f5
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 04:32:57 +0300

    Зроблено файл Program.cs для завдання 5

---
+++ lab-2/Lab2/Builder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            HeroBuilder heroBuilder = new HeroBuilder();
+            CharacterDirector heroDirector = new CharacterDirector(heroBuilder);
+            Character hero = heroDirector.ConstructHero();
+
+            EnemyBuilder enemyBuilder = new EnemyBuilder();
+            CharacterDirector enemyDirector = new CharacterDirector(enemyBuilder);
+            Character enemy = enemyDirector.ConstructEnemy();
+
+            Console.WriteLine("Персонаж1:");
+            hero.DisplayInfo();
+
+            Console.WriteLine("\nПерсонаж2:");
+            enemy.DisplayInfo();
+        }
+    }
+}
