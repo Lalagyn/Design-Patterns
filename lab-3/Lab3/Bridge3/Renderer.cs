5e1be01c7e0506f82cf35da99d39942c42819d28
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 16:44:48 +0300

    Створено  клас Renderer.cs для завдання 3

---
+++ lab-3/Lab3/Bridge3/Renderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge3
+{
+    public interface IRenderer
+    {
+        void Render(string shape);
+    }
+
+    // Реалізація растрового рендерингу
+    public class RasterRenderer : IRenderer
+    {
+        public void Render(string shape)
+        {
+            Console.WriteLine($"Малювання {shape} як пікселі");
+        }
+    }
+
+    // Реалізація векторного рендерингу
+    public class VectorRenderer : IRenderer
+    {
+        public void Render(string shape)
+        {
+            Console.WriteLine($"Малювання {shape} як лінії");
+        }
+    }
+}
