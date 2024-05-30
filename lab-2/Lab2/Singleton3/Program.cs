a6662ebb13027255eeeeb5f64525b0bf87b470ff
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 04:28:57 +0300

    Зроблено файл Program.cs для завдання 3

---
+++ lab-2/Lab2/Singleton3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                () => AuthenticateUser("user1", "password1"),
+                () => AuthenticateUser("user2", "password2"),
+                () => AuthenticateUser("user3", "password3")
+            );
+
+            Console.ReadLine();
+        }
+
+        static void AuthenticateUser(string username, string password)
+        {
+            Authenticator auth = Authenticator.Instance;
+            auth.Authenticate(username, password);
+        }
+    }
+}
