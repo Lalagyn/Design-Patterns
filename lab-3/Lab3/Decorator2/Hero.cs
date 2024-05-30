fdf9e9a90142fec38a181d3283a78503cc404694
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 16:42:40 +0300

    Створено  клас Hero.cs для завдання 2

---
+++ lab-3/Lab3/Decorator2/Hero.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator2
+{
+    public abstract class Hero
+    {
+        public abstract string GetDescription();
+        public abstract int GetPower();
+    }
+
+    public class Warrior : Hero
+    {
+        public override string GetDescription()
+        {
+            return "Warrior";
+        }
+
+        public override int GetPower()
+        {
+            return 10;
+        }
+    }
+
+    public class Mage : Hero
+    {
+        public override string GetDescription()
+        {
+            return "Mage";
+        }
+
+        public override int GetPower()
+        {
+            return 8;
+        }
+    }
+
+    public class Palladin : Hero
+    {
+        public override string GetDescription()
+        {
+            return "Palladin";
+        }
+
+        public override int GetPower()
+        {
+            return 12;
+        }
+    }
+}
