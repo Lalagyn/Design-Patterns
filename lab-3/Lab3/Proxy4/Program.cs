045f4a79ebe5a5a97c6b0636eb975d2c9d851b42
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 16:46:55 +0300

    Створено файл Program.cs для завдання 4

---
+++ lab-3/Lab3/Proxy4/Program.cs
@@ -0,0 +1,32 @@
+﻿using Proxy4;
+using System;
+using System.IO;
+using System.Text;
+
+namespace Proxy4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            string filePath = "example.txt";
+
+            // Створення файлу для тестування
+            System.IO.File.WriteAllText(filePath, "Hello\nWorld\nThis is a test file.");
+
+            // Використання проксі для логування
+            ITextReader checker = new SmartTextChecker(filePath);
+            checker.ReadFile();
+
+            // Використання проксі для обмеження доступу
+            ITextReader locker = new SmartTextReaderLocker(filePath, @"test\.txt$");
+            locker.ReadFile();
+
+            // Використання проксі для обмеження доступу (приклад з доступом)
+            ITextReader lockerWithAccess = new SmartTextReaderLocker(filePath, @"example\.txt$");
+            lockerWithAccess.ReadFile();
+        }
+    }
+}
