d973a426b12e8f94567868736f46c65c57fa9181
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 16:50:06 +0300

    Створено  клас SmartTextReaderLocker.cs для завдання 4

---
+++ lab-3/Lab3/Proxy4/SmartTextReaderLocker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Proxy4
+{
+    public class SmartTextReaderLocker : ITextReader
+    {
+        private SmartTextReader reader;
+        private Regex accessPattern;
+
+        public SmartTextReaderLocker(string filePath, string pattern)
+        {
+            this.reader = new SmartTextReader(filePath);
+            this.accessPattern = new Regex(pattern);
+        }
+
+        public char[][] ReadFile()
+        {
+            if (accessPattern.IsMatch(reader.FilePath))
+            {
+                Console.WriteLine("Access denied!");
+                return null;
+            }
+            else
+            {
+                return reader.ReadFile();
+            }
+        }
+    }
+}
