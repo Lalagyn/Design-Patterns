a8dbc4c4a421bd02cc7fb810c6c2def72f666c1e
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 16:43:45 +0300

    Створено файл Program.cs для завдання 3

---
+++ lab-3/Lab3/Bridge3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace Bridge3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            IRenderer rasterRenderer = new RasterRenderer();
+            IRenderer vectorRenderer = new VectorRenderer();
+
+            Shape circle = new Circle(rasterRenderer);
+            Shape square = new Square(vectorRenderer);
+            Shape triangle = new Triangle(rasterRenderer);
+
+            circle.Draw();
+            square.Draw();
+            triangle.Draw();
+        }
+    }
+}
