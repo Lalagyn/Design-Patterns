de1cd724d48282577c0eadd167430100e5b18701
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 04:27:53 +0300

    Зроблено клас Device.cs для завдання 2

---
+++ lab-2/Lab2/Abstractfactory2/Device.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Abstractfactory
+{
+    public interface IDevice
+    {
+        string GetDetails();
+    }
+
+    public class Laptop : IDevice
+    {
+        private string brand;
+
+        public Laptop(string brand)
+        {
+            this.brand = brand;
+        }
+
+        public string GetDetails()
+        {
+            return $"{brand} Laptop";
+        }
+    }
+
+    public class Netbook : IDevice
+    {
+        private string brand;
+
+        public Netbook(string brand)
+        {
+            this.brand = brand;
+        }
+
+        public string GetDetails()
+        {
+            return $"{brand} Netbook";
+        }
+    }
+
+    public class EBook : IDevice
+    {
+        private string brand;
+
+        public EBook(string brand)
+        {
+            this.brand = brand;
+        }
+
+        public string GetDetails()
+        {
+            return $"{brand} EBook";
+        }
+    }
+
+    public class Smartphone : IDevice
+    {
+        private string brand;
+
+        public Smartphone(string brand)
+        {
+            this.brand = brand;
+        }
+
+        public string GetDetails()
+        {
+            return $"{brand} Smartphone";
+        }
+    }
+}
