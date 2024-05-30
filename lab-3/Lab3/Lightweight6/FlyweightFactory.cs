9e7209222bfc80f2c819d3917c16de6768fa10a2
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 16:53:20 +0300

    Створено  клас FlyweightFactory.cs для завдання 6

---
+++ lab-3/Lab3/Lightweight6/FlyweightFactory.cs
@@ -0,0 +1,24 @@
+﻿using Composer5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lightweight6
+{
+    public class FlyweightFactory
+    {
+        private readonly Dictionary<string, LightElementNode> _flyweights = new Dictionary<string, LightElementNode>();
+
+        public LightElementNode GetFlyweight(string tagName, DisplayType display, CloseType close)
+        {
+            string key = $"{tagName}_{display}_{close}";
+            if (!_flyweights.ContainsKey(key))
+            {
+                _flyweights[key] = new LightElementNode(tagName, display, close);
+            }
+            return _flyweights[key];
+        }
+    }
+}
