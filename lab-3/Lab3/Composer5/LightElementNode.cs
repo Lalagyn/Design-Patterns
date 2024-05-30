bca307a1d564d9fb4437d444a09fe6e7f2751a91
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 16:51:35 +0300

    Створено  клас LightElementNode.cs для завдання 5

---
+++ lab-3/Lab3/Composer5/LightElementNode.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composer5
+{
+    public enum DisplayType
+    {
+        Block,
+        Inline
+    }
+
+    public enum CloseType
+    {
+        SelfClosing,
+        Normal
+    }
+
+    public class LightElementNode : LightNode
+    {
+        public string TagName { get; set; }
+        public DisplayType Display { get; set; }
+        public CloseType Close { get; set; }
+        public List<string> CssClasses { get; set; }
+        public List<LightNode> Children { get; set; }
+
+        public LightElementNode(string tagName, DisplayType display, CloseType close)
+        {
+            TagName = tagName;
+            Display = display;
+            Close = close;
+            CssClasses = new List<string>();
+            Children = new List<LightNode>();
+        }
+
+        public override string OuterHTML
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append($"<{TagName}");
+
+                if (CssClasses.Count > 0)
+                {
+                    sb.Append($" class=\"{string.Join(" ", CssClasses)}\"");
+                }
+
+                if (Close == CloseType.SelfClosing)
+                {
+                    sb.Append(" />");
+                }
+                else
+                {
+                    sb.Append(">");
+                    sb.Append(InnerHTML);
+                    sb.Append($"</{TagName}>");
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        public override string InnerHTML
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var child in Children)
+                {
+                    sb.Append(child.OuterHTML);
+                }
+                return sb.ToString();
+            }
+        }
+
+        public void AddChild(LightNode child)
+        {
+            Children.Add(child);
+        }
+    }
+}
