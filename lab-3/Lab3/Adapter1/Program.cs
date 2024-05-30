22bdd9e1dddea4dc79add71ec2026385e0862598
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 16:40:23 +0300

    Створено файл Program.cs для завдання 1

---
+++ lab-3/Lab3/Adapter1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace Adapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Logger consoleLogger = new Logger();
+            consoleLogger.Log("Це логове повідомлення");
+            consoleLogger.Error("Це повідомлення про помилку");
+            consoleLogger.Warn("Це попереджувальне повідомлення");
+
+            string filePath = "log.txt";
+            FileLoggerAdapter fileLogger = new FileLoggerAdapter(filePath);
+            fileLogger.Log("Це логове повідомлення (файл)");
+            fileLogger.Error("Це повідомлення про помилку (файл)");
+            fileLogger.Warn("Це попереджувальне повідомлення (файл)");
+
+            Console.WriteLine($"Повідомлення збережено у файлі {filePath}");
+        }
+    }
+}
