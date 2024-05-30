e1c959daab733b730b49b4d22b526bd290a64c1e
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 16:48:30 +0300

    Створено  клас SmartTextChecker.cs для завдання 4

---
+++ lab-3/Lab3/Proxy4/SmartTextChecker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy4
+{
+    public class SmartTextChecker : ITextReader
+    {
+        private SmartTextReader reader;
+
+        public SmartTextChecker(string filePath)
+        {
+            this.reader = new SmartTextReader(filePath);
+        }
+
+        public char[][] ReadFile()
+        {
+            Console.WriteLine($"Відкриття файлу: {reader.FilePath}");
+            char[][] content = reader.ReadFile();
+            Console.WriteLine($"Прочитання файлу: {reader.FilePath}");
+            Console.WriteLine($"Закриття файлу: {reader.FilePath}");
+
+            int lineCount = content.Length;
+            int charCount = 0;
+            foreach (var line in content)
+            {
+                charCount += line.Length;
+            }
+
+            Console.WriteLine($"Кількість рядків: {lineCount}");
+            Console.WriteLine($"Кількість символів: {charCount}");
+
+            return content;
+        }
+    }
+}
