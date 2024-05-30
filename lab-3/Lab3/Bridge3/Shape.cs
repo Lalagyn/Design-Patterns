7f1f597b27db205e1436622758601e3a3655fde7
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 16:45:14 +0300

    Створено  клас Shape.cs для завдання 3

---
+++ lab-3/Lab3/Bridge3/Shape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge3
+{
+    public abstract class Shape
+    {
+        protected IRenderer renderer;
+
+        protected Shape(IRenderer renderer)
+        {
+            this.renderer = renderer;
+        }
+
+        public abstract void Draw();
+    }
+}
