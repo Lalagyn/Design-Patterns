e3da8330459665d547585700243aa5ce5f05bcda
Віталій Король <vitaliykorol444@gmail.com>
2024-05-30 04:27:08 +0300

    Зроблено файл Program.cs для завдання 2

---
+++ lab-2/Lab2/Abstractfactory2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Abstractfactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IBrandFactory iproneFactory = new IProneFactory();
+            IBrandFactory kiaomiFactory = new KiaomiFactory();
+            IBrandFactory balaxyFactory = new BalaxyFactory();
+
+            IDevice iproneLaptop = iproneFactory.CreateLaptop();
+            IDevice kiaomiSmartphone = kiaomiFactory.CreateSmartphone();
+            IDevice balaxyEBook = balaxyFactory.CreateEBook();
+
+            Console.WriteLine(iproneLaptop.GetDetails());
+            Console.WriteLine(kiaomiSmartphone.GetDetails());
+            Console.WriteLine(balaxyEBook.GetDetails());
+        }
+    }
+}
